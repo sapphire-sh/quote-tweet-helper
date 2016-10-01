e524f8c91bae142783ef28b126bc366a5988f7fc
sapphire <info@sapphire.sh>
2016-10-01 23:15:49 +0900

    implement parsing

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ let compression = require('compression');
 
 let app = express();
 
+let request = require('request');
+let cheerio = require('cheerio');
+
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -26,7 +29,49 @@ app.use(compression());
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('../public'));
-app.use('/', require('./router'));
+
+app.get('/', (req, res) => {
+	console.log(req.session.oauth);
+	res.render('index', {
+		oauth: req.session.oauth
+	});
+});
+
+app.post('/i', (req, res) => {
+	console.log(req.body.url);
+	res.redirect(`/i/${req.body.url.split('/').pop()}`);
+});
+	
+app.get('/i/:id', (req, res) => {
+	request(`https:\/\/twitter.com/quote_helper\/status\/${req.params.id}`, (err, response, html) => {
+		if(err) {
+			res.json(err);
+		}
+		else {
+			let $ = cheerio.load(html);
+			
+			let title = $('meta[property="og:title"]').attr('content');
+			title = title.split(' ');
+			title.pop();
+			title.pop();
+			title = title.join(' ');
+			let description = $('meta[property="og:description"]').attr('content');
+			let image = $('.js-action-profile-avatar').attr('src');
+			image = image.split('.');
+			let extension = image.pop();
+			image = image.join('.').split('_');
+			image.pop();
+			image = `${image.join('_')}.${extension}`;
+			
+			res.render('card', {
+				id: req.params.id,
+				title: title,
+				description: description,
+				image: image
+			});
+		}
+	});
+});
 
 app.listen(8022);
 
